b8538cfd57b85212902cb355308143a12e4cfd9f
Bitsulia Dmitry <gaudima@gmail.com>
2016-03-11 20:25:07 +0300

    fixed hw1

--- HW1/src/ru.ifmo.ctddev.biculya.walk/Md5Hasher.java
+++ HW1/src/ru.ifmo.ctddev.biculya.walk/Md5Hasher.java
@@ -12,17 +12,23 @@ import java.security.NoSuchAlgorithmException;
  * Created by gaudima on 2/10/16.
  */
 public class Md5Hasher {
-    public String hashFile(Path filePath) throws IOException, NoSuchAlgorithmException {
-        MessageDigest digest = MessageDigest.getInstance("MD5");
-        try(InputStream is = Files.newInputStream(filePath);
-            DigestInputStream dis = new DigestInputStream(is, digest)) {
-            while(dis.available() > 0) {
+    public String hashFile(Path filePath) throws IOException {
+        MessageDigest digest;
+        try {
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            System.out.println("No MD5 algorithm.");
+            return "";
+        }
+        try (InputStream is = Files.newInputStream(filePath);
+             DigestInputStream dis = new DigestInputStream(is, digest)) {
+            while (dis.available() > 0) {
                 dis.read();
             }
         }
         StringBuilder ret = new StringBuilder();
         byte[] d = digest.digest();
-        for(int i = 0; i < d.length; i++) {
+        for (int i = 0; i < d.length; i++) {
             ret.append(String.format("%02X", d[i]));
         }
         digest.reset();
--- HW1/src/ru.ifmo.ctddev.biculya.walk/Walk.java
+++ HW1/src/ru.ifmo.ctddev.biculya.walk/Walk.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 /**
  * Created by gaudima on 2/10/16.
@@ -16,10 +13,11 @@ public class Walk {
     private static BufferedReader br;
     private static BufferedWriter bw;
     private static Md5Hasher hasher = new Md5Hasher();
+
     private static void walkRecursive(Path path) throws IOException {
-        if(Files.isDirectory(path)) {
+        if (Files.isDirectory(path)) {
             try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
-                for(Path p : directoryStream) {
+                for (Path p : directoryStream) {
                     walkRecursive(p);
                 }
             }
@@ -27,7 +25,7 @@ public class Walk {
             String hash;
             try {
                 hash = hasher.hashFile(path);
-            } catch (Exception e) {
+            } catch (IOException e) {
                 hash = "00000000000000000000000000000000";
             }
             bw.write(hash);
@@ -36,24 +34,35 @@ public class Walk {
             bw.write("\n");
         }
     }
+
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Wrong number of arguments.");
+            return;
+        }
         try {
             br = Files.newBufferedReader(Paths.get(args[0]), Charset.forName("UTF-8"));
             bw = Files.newBufferedWriter(Paths.get(args[1]), Charset.forName("UTF-8"));
+
             String path = br.readLine();
 
-            while(path != null) {
+            while (path != null) {
                 walkRecursive(Paths.get(path));
                 path = br.readLine();
             }
-            br.close();
-            bw.close();
-        } catch(IOException e) {
-            System.out.println(e.getMessage());
+        } catch (NoSuchFileException e) {
+            System.out.println("No such file: " + e.getFile());
+        } catch (IOException e) {
+            System.out.println("Error reading input or writing output file.");
+        } finally {
             try {
-                br.close();
-                bw.close();
-            } catch (IOException ex) {}
+                if (br != null) {
+                    br.close();
+                }
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException ex) { }
         }
     }
 }
